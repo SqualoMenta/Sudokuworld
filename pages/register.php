941516d7380bf02d1f3e0a7657d74bc857d5b8d0
Andrea Samori <andrea.samori3@studio.unibo.it>
2024-12-28 16:24:50 +0100

    modified register.php

--- pages/register.php
+++ pages/register.php
@@ -4,25 +4,28 @@ include_once("../includes/bootstrap.php");
 include_once("../includes/functions.php");
 
 if (isset($_POST["name"]) && isset($_POST["mail"]) && isset($_POST["password"])) {
-    // $login_result = $db->checkLogin($_POST["username"], $_POST["password"]);
-    if(count($db->getUser($_POST["mail"]))>0){
+
+    if (count($db->getUser($_POST["mail"])) > 0) {
         $register_error = "Errore! Email gia' utilizzata!";
     } else {
         unset($register_error);
-        registerLoggedUser($login_result[0]);
+        $db->registerUser($_POST["name"], $_POST["mail"], $_POST["password"]);
     }
 }
 
 
 ?>
 <form action="#" method="POST">
-    <h2>Login</h2>
-    <?php if (isset($login_error)): ?>
-        <p><?php echo $login_error ?></p>
+    <h2>Registrazione</h2>
+    <?php if (isset($register_error)): ?>
+        <p><?php echo $register_error ?></p>
     <?php endif; ?>
     <ul>
         <li>
             <label for="username">Username:</label><input type="text" id="username" name="username" />
+        </li>
+        <li>
+            <label for="email">Email:</label><input type="email" id="email" name="email" />
         </li>
         <li>
             <label for="password">Password:</label><input type="password" id="password" name="password" />
