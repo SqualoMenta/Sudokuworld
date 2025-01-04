db21d66c7f3fef6c9af7391f2d30aa10213a3f7d
davi-bart <davide30.bartoli@gmail.com>
2025-01-04 15:56:38 +0100

    some css

--- pages/logout.php
+++ pages/logout.php
@@ -6,9 +6,11 @@ include_once("../includes/functions.php");
 logout();
 include("../includes/header.php");
 ?>
-
-<h1>Logout effettuato</h1>
-
+<div class="d-flex justify-content-center align-items-center vh-100">
+    <div class="container text-center">
+        <h1>Logout effettuato</h1>
+    </div>
+</div>
 <?php
 include("../includes/footer.php");
 ?>
