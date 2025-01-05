18355875d3f9072ec394195c4eaf782a67713417
davi-bart <davide30.bartoli@gmail.com>
2025-01-05 14:08:20 +0100

    add todo

--- pages/add_product.php
+++ pages/add_product.php
@@ -15,12 +15,12 @@ if (isset($_FILES["image"]) && $_FILES["image"]["error"] == 0) {
 }
 
 if (isset($_POST["name"]) && isset($_POST["description"]) && isset($_POST["price"]) && isset($_POST["discount"])) {
+    // TODO: mi sa che sta roba si può rompere
     $imageName = $imageName ?? null;
     $_POST["price"] = 100 * $_POST["price"];
     $db->seller->insertProduct($_POST["name"], $_POST["description"], $_POST["price"], $imageName, $_SESSION["email"], $_POST["discount"]);
     echo "Prodotto inserito con successo!";
 }
-
 $product = new Product(null, '', 0, '', '', $_SESSION["email"], 0);
 include("../includes/header.php");
 ?>
