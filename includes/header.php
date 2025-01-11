3b2d5e67ba7bc8f711651f13f99d27b50198c57b
davi-bart <davide30.bartoli@gmail.com>
2025-01-11 15:04:09 +0100

    add icons in navbar

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,41 @@
-<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- Bootstrap JS Bundle with Popper (for tooltips, popovers, etc.) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 <link href="/assets/css/style.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <!-- Logo -->
         <a class="navbar-brand" href="/pages/home.php">SudokuWorld</a>
 
-        <!-- Search Form -->
         <form class="d-flex" action="/pages/search.php" method="post">
             <input class="form-control me-2" type="text" name="searched-product" placeholder="Search" value="<?php if (isset($_POST['searched-product'])) echo $_POST['searched-product']; ?>">
             <button class="btn btn-outline-success" type="submit">Cerca</button>
         </form>
 
-        <!-- Navbar Icons -->
         <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/pages/sudoku.php">Griglia</a>
+                <a class="nav-link" href="/pages/sudoku.php">
+                    <i class="fas fa-th"></i> Griglia
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/pages/wishlist.php">Cuore</a>
+                <a class="nav-link" href="/pages/wishlist.php">
+                    <i class="fas fa-heart"></i> Cuore
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/pages/cart.php">Carrello</a>
+                <a class="nav-link" href="/pages/cart.php">
+                    <i class="fas fa-shopping-cart"></i> Carrello
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/pages/profile.php">Profilo</a>
+                <a class="nav-link" href="/pages/profile.php">
+                    <i class="fas fa-user"></i> Profilo
+                </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/pages/notifications.php">Notifiche</a>
+                <a class="nav-link" href="/pages/notifications.php">
+                    <i class="fas fa-bell"></i> Notifiche
+                </a>
             </li>
         </ul>
     </div>
