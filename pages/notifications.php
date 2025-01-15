45dd6ed6265bfbbd9937f3e3e3bc6887c886f73a
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2025-01-15 18:03:20 +0100

    no more scrollbar

--- pages/notifications.php
+++ pages/notifications.php
@@ -22,24 +22,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 include_once("../includes/header.php");
 ?>
-
-<div class="container my-4">
-    <h1 class="mb-4">Notifiche</h1>
-    <div class="list-group">
-        <?php foreach ($notifications as $notify) : ?>
-            <div class="list-group-item d-flex justify-content-between align-items-center <?= $notify['seen'] ? 'list-group-item-secondary' : '' ?>">
-                <div>
-                    <h5 class="mb-1"><?= htmlspecialchars($notify["title"]) ?></h5>
-                    <p class="mb-1"><?= htmlspecialchars($notify["description"]) ?></p>
+<main>
+    <div class="container my-4">
+        <h1 class="mb-4">Notifiche</h1>
+        <div class="list-group">
+            <?php foreach ($notifications as $notify) : ?>
+                <div class="list-group-item d-flex justify-content-between align-items-center <?= $notify['seen'] ? 'list-group-item-secondary' : '' ?>">
+                    <div>
+                        <h5 class="mb-1"><?= htmlspecialchars($notify["title"]) ?></h5>
+                        <p class="mb-1"><?= htmlspecialchars($notify["description"]) ?></p>
+                    </div>
+                    <form method="POST" action="#">
+                        <input type="hidden" name="mark_as_read" value="1">
+                        <input type="hidden" name="id_notification" value="<?= htmlspecialchars($notify['id_notification']) ?>">
+                        <?php if (!$notify['seen']) : ?>
+                            <button type="submit" class="btn btn-primary btn-sm">Mark as Read</button>
+                        <?php endif; ?>
+                    </form>
                 </div>
-                <form method="POST" action="#">
-                    <input type="hidden" name="mark_as_read" value="1">
-                    <input type="hidden" name="id_notification" value="<?= htmlspecialchars($notify['id_notification']) ?>">
-                    <?php if (!$notify['seen']) : ?>
-                        <button type="submit" class="btn btn-primary btn-sm">Mark as Read</button>
-                    <?php endif; ?>
-                </form>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
-</div>
+</main>
